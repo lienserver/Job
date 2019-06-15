d48b998b02df4492af7bdaa69b4b7a90986a534e
ponpono <gw7121@naver.com>
2019-06-16 05:46:36 +0900

    .

--- src/main/java/lien/job/command/parameter/JobType.java
+++ src/main/java/lien/job/command/parameter/JobType.java
@@ -1,5 +1,26 @@
 package lien.job.command.parameter;
 
-public class JobType {
+import com.gnurung.gunucommandmodule.module.ParameterType;
+
+import lien.job.JobPl;
+
+public class JobType implements ParameterType
+{
+	@Override
+	public String regex(Object sender, String s)
+	{
+		if(JobPl.getInstance().getJob(s) == null) return "($^)";
+		else return "("+s+")";
+	}
+	@Override
+	public Object getObject(Object sender, String s)
+	{
+		return JobPl.getInstance().getJob(s);
+	}
+	@Override
+	public String getName() {
+		return "job";
+	}
+
 
 }
--- src/main/java/lien/job/command/parameter/StatType.java
+++ src/main/java/lien/job/command/parameter/StatType.java
@@ -1,5 +1,29 @@
 package lien.job.command.parameter;
 
-public class StatType {
+import com.gnurung.gunucommandmodule.module.ParameterType;
+
+import lien.job.api.Stat;
+
+public class StatType implements ParameterType
+{
+
+	@Override
+	public String getName() 
+	{
+		return "stat";
+	}
+
+	@Override
+	public Object getObject(Object arg0, String arg1)
+	{
+		return Stat.getByLocalizedName(arg1);
+	}
+
+	@Override
+	public String regex(Object arg0, String arg1) 
+	{
+		if(Stat.getByLocalizedName(arg1) == null) return "$^";
+		else return arg1;
+	}
 
 }
